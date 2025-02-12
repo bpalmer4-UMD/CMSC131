f5096b56ef6aef2417b60ff69afc4d56a6c6d0f7
bpalmer4-UMD <bpalmer4@terpmail.umd.edu>
2025-02-12 13:29:19 -0500

    Update Utilities.java

--- RecursiveMethods/Utilities.java
+++ RecursiveMethods/Utilities.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Utilities {
 
+	// Returns a string where characters are separated by the specified 
+	// delimiter character. A string with a single character will not have
+	// a delimiter added.
 	public static String addDelimiter(String str, char delimeter) {
 		if (str.length() == 1) {
 			return str;
@@ -11,7 +14,7 @@ public class Utilities {
 		return str.substring(0,1) + delimeter + addDelimiter(str.substring(1,str.length()), delimeter);
 	}
 	
-
+	// Returns a string with the digits (if any) present in the str parameter.
 	public static String getDigits(String str) {
 		if (str.length() == 0) {
 			return "";
@@ -23,10 +26,12 @@ public class Utilities {
 	}
 
 	
+	// Replaces the target character in the array with the specified replacement.
 	public static void replaceCharacter(char[] array, char target, char replacement) {
 		replaceCharHelper(array, target, replacement, 0);
 	}
 	
+	// Helper method for replaceCharacter()
 	private static void replaceCharHelper(char[] array, char target, char replacement, int i) {
 		if (i > (array.length - 1)) {
             return;
@@ -37,11 +42,12 @@ public class Utilities {
         replaceCharHelper(array, target, replacement, i + 1);
 	}
 
-	
+	// Returns the sum of even values (if any) present in the array parameter.
 	public static int getSumEven(int[] array) {
 		return getSumHelper(array, 0);
 	}
 	
+	// Helper method for getSumEven()
 	private static int getSumHelper(int[] array, int i) {
 		if (i > (array.length - 1)) {
 			return 0;
@@ -52,13 +58,15 @@ public class Utilities {
 		return getSumHelper(array, i + 1);
 	}
 
-	
+	// This method returns an ArrayList with the indices of rows of the 
+	// two-dimensional array having a length that corresponds to rowLength.
 	public static ArrayList<Integer> getListRowIndices(int[][] array, int rowLength) {
 		ArrayList<Integer> ind = new ArrayList<>();
         getListRowHelper(array, rowLength, ind, 0);
         return ind;
 	}
 	
+	// Helper method for getListRowIndices()
 	private static void getListRowHelper(int[][] array, int rowLength, ArrayList<Integer> ind, int i) {
 		if (i > (array.length - 1)) {
 			return;
@@ -69,11 +77,13 @@ public class Utilities {
 		getListRowHelper(array, rowLength, ind, i + 1);
 	}
 
-	
+	// This method replaces a character and instances of the character that are
+	// adjacent to it, with a replacement character.
 	public static int replaceCells(char[][] array, int x, int y, char target, char replacement) {
 		return replaceHelper(array, x, y, target, replacement);
 	}
 	
+	// Helper method for replaceCells()
 	private static int replaceHelper(char[][] array, int x, int y, char target, char replacement) {
 		int ct = 0;
 		
@@ -97,6 +107,8 @@ public class Utilities {
         return ct;
 	}
 	
+	// This method validates the x y  pair of the array
+	// passed in to replaceHelper()
 	private static boolean validXY(char[][] array, int x, int y) {
         return x >= 0 && x < array.length && y >= 0 && y < array[x].length;
     }
